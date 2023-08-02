9e6a9b3a803c819b7c49a2e3d042e0ea2ab41b73
Afif Khoirul Amri <khoirulamr9@gmail.com>
2023-08-02 23:38:11 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>hello world</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat expedita ullam nesciunt architecto sapiente quaerat, doloremque vero delectus ducimus voluptatum numquam hic exercitationem fuga iusto ex illo iste omnis cupiditate?</p>
 </body>
 </html>
